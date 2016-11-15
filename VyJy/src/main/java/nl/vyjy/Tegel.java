41ebce754c3d52389e60362c7b0e8e477b22dbb5
Jenny den Ouden <jenny.den.ouden@gmail.com>
2016-11-15 16:46:04 +0100

    Onaffe klasse voor Tegel

---
+++ VyJy/src/main/java/nl/vyjy/Tegel.java
@@ -0,0 +1,14 @@
+package nl.vyjy;
+
+public class Tegel {
+
+	private Windstreek noord, oost, zuid, west;
+	
+	public Tegel(Windstreek noord, Windstreek oost, Windstreek zuid, Windstreek west){
+		this.noord = noord;
+		this.oost = oost;
+		this.zuid = zuid;
+		this.west = west;
+	}
+	
+}
