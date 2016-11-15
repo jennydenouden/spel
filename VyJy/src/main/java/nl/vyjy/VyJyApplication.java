4ee190fbe0cb10f3f9c739281863ec24b656c779
Jenny den Ouden <jenny.den.ouden@gmail.com>
2016-11-15 12:29:02 +0100

    Maven project aangemaakt met spring.

---
+++ VyJy/src/main/java/nl/vyjy/VyJyApplication.java
@@ -0,0 +1,12 @@
+package nl.vyjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyJyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyJyApplication.class, args);
+	}
+}
