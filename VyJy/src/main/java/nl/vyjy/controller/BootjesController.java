4af2fbe7c279aaf94824f6fedc7542e66dc68741
Jenny den Ouden <jenny.den.ouden@gmail.com>
2016-11-16 15:59:34 +0100

    BootjesController maakt nu de 7 bootjes uit het excel bestand aan en stopt die in de database

--- VyJy/src/main/java/nl/vyjy/controller/BootjesController.java
+++ VyJy/src/main/java/nl/vyjy/controller/BootjesController.java
@@ -1,9 +1,14 @@
 package nl.vyjy.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import nl.vyjy.Bootje;
 
 @Controller
 public class BootjesController {
@@ -12,8 +17,44 @@ public class BootjesController {
 	private BootjesRepository repo;
 	
 	@RequestMapping("/bootjes")
-	public @ResponseBody String initBootjes(){		
-		return "Deze pagina stopt de bootjes in de database";
+	public String initBootjes(Model model){		
+		repo.deleteAll();
+		ArrayList<Bootje> bootjes = getAllBootjes();
+		for(Bootje b : bootjes){
+			repo.save(b);
+		}
+		
+		model.addAttribute("bootjes", bootjes);
+		return "showBootjes";
+	}
+	
+	
+	private ArrayList<Bootje> getAllBootjes(){
+		ArrayList<Bootje> bootjes = new ArrayList<>();
+		
+		Bootje b;
+		b = new Bootje(3, 2, 1, 0);
+		bootjes.add(b);
+		
+		b = new Bootje(6, 2, 1, 2);
+		bootjes.add(b);
+		
+		b = new Bootje(4, 0, 2, 2);
+		bootjes.add(b);
+		
+		b = new Bootje(2, 1, 0, 1);
+		bootjes.add(b);
+		
+		b = new Bootje(2, 0, 1, 1);
+		bootjes.add(b);
+		
+		b = new Bootje(3, 2, 0, 1);
+		bootjes.add(b);
+		
+		b = new Bootje(5, 1, 1, 3);
+		bootjes.add(b);
+		
+		return bootjes;
 	}
 	
 }
