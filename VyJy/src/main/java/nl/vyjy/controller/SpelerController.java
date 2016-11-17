1d3e21503e424363e0d8f41ffe49c64e713c7f08
Feia Matthijssen <feia.matthijssen@gmail.com>
2016-11-17 09:29:26 +0100

    SpelerController gemaakt

--- VyJy/src/main/java/nl/vyjy/controller/SpelerController.java
+++ VyJy/src/main/java/nl/vyjy/controller/SpelerController.java
@@ -1,5 +1,8 @@
 package nl.vyjy.controller;
 
+import java.util.ArrayList;
+import nl.vyjy.Speler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpelerController {
     
+    @Autowired
+    private SpelerRepository repo;
+    
     @RequestMapping("/speler")
     public String showSpeler(Model model){
-        model.addAttribute("inventaris", );
+        
+        if(repo.count() == 0){
+            ArrayList<Speler> spelers = getAllSpelers();
+            for(Speler s : spelers){
+                    repo.save(s);
+            }
+        }
+        
+        model.addAttribute("spelers", repo.findAll());
         return "showSpeler";
+    }
+
+    private ArrayList<Speler> getAllSpelers() {
+        ArrayList<Speler> spelers = new ArrayList<>();
+        
+        Speler s;
+        s = new Speler("Jenny");
+        spelers.add(s);
+        s = new Speler("Feia");
+        spelers.add(s);
+        
+        return spelers;
     }
 }
