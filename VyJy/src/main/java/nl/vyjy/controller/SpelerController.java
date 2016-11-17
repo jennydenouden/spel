a2e4de9f8652fe218aa8e5e7bde03da4e337898d
Feia Matthijssen <feia.matthijssen@gmail.com>
2016-11-17 11:18:18 +0100

    het is nu mogelijk om 1 token op te tellen of af te trekken op de player pagina, dit kan niet onder 0

--- VyJy/src/main/java/nl/vyjy/controller/SpelerController.java
+++ VyJy/src/main/java/nl/vyjy/controller/SpelerController.java
@@ -2,11 +2,14 @@ package nl.vyjy.controller;
 
 
 import java.util.ArrayList;
+import javax.servlet.http.HttpServletResponse;
 import nl.vyjy.Speler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SpelerController {
@@ -26,6 +29,60 @@ public class SpelerController {
         
         model.addAttribute("spelers", repo.findAll());
         return "showSpeler";
+    }
+    
+    @RequestMapping(value="/visjebij{id}", method=RequestMethod.GET)
+    public String krijgVisjes(@PathVariable long id, HttpServletResponse response){
+        Speler s = repo.findOne(id);
+        s.setVisjes(s.getVisjes() + 1);
+        repo.save(s);
+        return "redirect:/speler";
+    }
+    
+        @RequestMapping(value="/banaanbij{id}", method=RequestMethod.GET)
+    public String krijgBanaan(@PathVariable long id, HttpServletResponse response){
+        Speler s = repo.findOne(id);
+        s.setBananen(s.getBananen() + 1);
+        repo.save(s);
+        return "redirect:/speler";
+    }
+    
+        @RequestMapping(value="/schelpbij{id}", method=RequestMethod.GET)
+    public String krijgSchelpen(@PathVariable long id, HttpServletResponse response){
+        Speler s = repo.findOne(id);
+        s.setSchelpen(s.getSchelpen() + 1);
+        repo.save(s);
+        return "redirect:/speler";
+    }
+    
+        @RequestMapping(value="/visjeaf{id}", method=RequestMethod.GET)
+    public String geefVisjes(@PathVariable long id, HttpServletResponse response){
+        Speler s = repo.findOne(id);
+        if(s.getVisjes() > 0 ){
+            s.setVisjes(s.getVisjes() - 1);
+            repo.save(s);
+        }
+        return "redirect:/speler";
+    }
+    
+        @RequestMapping(value="/banaanaf{id}", method=RequestMethod.GET)
+    public String geefBanaan(@PathVariable long id, HttpServletResponse response){
+        Speler s = repo.findOne(id);
+        if(s.getBananen() > 0){
+            s.setBananen(s.getBananen() - 1);
+            repo.save(s);
+        }
+        return "redirect:/speler";
+    }
+    
+        @RequestMapping(value="/schelpaf{id}", method=RequestMethod.GET)
+    public String geefSchelpen(@PathVariable long id, HttpServletResponse response){
+        Speler s = repo.findOne(id);
+        if(s.getSchelpen() > 0){
+            s.setSchelpen(s.getSchelpen() - 1);
+            repo.save(s);
+        }
+        return "redirect:/speler";
     }
 
     private ArrayList<Speler> getAllSpelers() {
