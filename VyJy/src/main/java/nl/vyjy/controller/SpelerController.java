b244623801c962338632f75fba597fb88402ec9f
Jenny den Ouden <jenny.den.ouden@gmail.com>
2016-11-17 09:28:00 +0100

    Wat aanpassingen gemaakt in speler, spelercontroller en spelerRepository (nog niet getest)

--- VyJy/src/main/java/nl/vyjy/controller/SpelerController.java
+++ VyJy/src/main/java/nl/vyjy/controller/SpelerController.java
@@ -1,5 +1,6 @@
 package nl.vyjy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpelerController {
     
+	@Autowired
+	private SpelerRepository repo;
+	
     @RequestMapping("/speler")
     public String showSpeler(Model model){
-        model.addAttribute("inventaris", );
+       // model.addAttribute("inventaris", );
         return "showSpeler";
     }
 }
