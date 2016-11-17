e105f96e9b24b9f8b5ef0fe5d518cfb23e2a7ba6
Feia Matthijssen <feia.matthijssen@gmail.com>
2016-11-17 16:18:37 +0100

    showBord tekent nu een grid van dummy afbeeldingen

--- VyJy/src/main/webapp/js/showBord.js
+++ VyJy/src/main/webapp/js/showBord.js
@@ -7,23 +7,21 @@ function draw(){
         var canvasHeight = canvas.height;
         var gridSize = 25;
         var tileSize = canvasWidth / gridSize;
-                      
-        var x = 0;
-        var y = 0;
-        while (x < canvasWidth){
-            while (y < canvasHeight){
-                ctx.strokeRect(x,y,tileSize,tileSize);
-                
-                y += tileSize;
-            }
-            y = 0;
-            x += tileSize; 
-        }
-        
+                             
         var img = new Image();
         img.onload = function(){
-            ctx.drawImage(img,0,0);
+            var x = 0;
+            var y = 0;
+            while (x < canvasWidth){
+                while (y < canvasHeight){
+                    ctx.strokeRect(x,y,tileSize,tileSize);
+                    ctx.drawImage(img,x,y,tileSize,tileSize);
+                    y += tileSize;
+                }
+                y = 0;
+                x += tileSize; 
+        }
         };
-        img.src = '/webapp/images/leegvakje.jpg';
+        img.src = '/images/leegvakje.jpg';
     }
 }
