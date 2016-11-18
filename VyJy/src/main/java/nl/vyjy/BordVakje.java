58388e96a27c8171dfb060c2e4693d908ab69e95
Student <Student@C0022.isential.local>
2016-11-18 12:26:31 +0100

    BordVakje aangemaakt. Deze heeft nu ook een repository in MySQL

--- VyJy/src/main/java/nl/vyjy/BordVakje.java
+++ VyJy/src/main/java/nl/vyjy/BordVakje.java
@@ -1,8 +1,46 @@
 package nl.vyjy;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class BordVakje {
+    
+    @Id
     private long id;
     private int xPos;
     private int yPos;
-    private Tegel tegel;
+    //private Tegel tegel;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+//    public Tegel getTegel() {
+//        return tegel;
+//    }
+//
+//    public void setTegel(Tegel tegel) {
+//        this.tegel = tegel;
+//    }
 }
