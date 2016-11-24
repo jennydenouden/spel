929f7b4821929c693ea750c3b6a7ae323bf3ed2a
Jenny den Ouden <jenny.den.ouden@gmail.com>
2016-11-24 13:35:29 +0100

    naam plaatjes aangepast in setTegels

--- VyJy/src/main/java/nl/vyjy/SetTegels.java
+++ VyJy/src/main/java/nl/vyjy/SetTegels.java
@@ -18,30 +18,30 @@ public class SetTegels {
 		
 		Tegel t;
 		//1x pad pad pad pad
-		t = new Tegel(new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG));
+		t = new Tegel(new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG), "images/tegels/bbbb.png");
 		alleTegels.add(t);
 		
 		//3x pad pad pad water
 		for(int i = 0; i < 3; i++){
-			t = new Tegel(new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.WATER));
+			t = new Tegel(new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.WATER), "images/tegels/bbbw.png");
 			alleTegels.add(t);
 		}
 		
 		//5x pad water pad water
 		for(int i = 0; i < 5; i++){
-			t = new Tegel(new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.WATER));
+			t = new Tegel(new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.WATER), "images/tegels/bwbw_2.png");
 			alleTegels.add(t);
 		}
 		
 		//2x pad water water water
 		for(int i = 0; i < 2; i++){
-			t = new Tegel(new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER));
+			t = new Tegel(new Windstreek(Ondergrond.BRUG), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER), "images/tegels/bwww.png");
 			alleTegels.add(t);
 		}
 		
 		//2x water water water water
 		for(int i = 0; i < 2; i++){
-			t = new Tegel(new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER));
+			t = new Tegel(new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER), new Windstreek(Ondergrond.WATER), "images/tegels/wwww.png");
 			alleTegels.add(t);
 		}
 		
