5c5fd55f6b5ad197b9e86e9f00a9160ba0234bfe
Feia <feia.matthijssen@gmail.com>
2016-11-29 15:06:04 +0100

    bordvakje is ook niet gebruikt

--- VyJy/src/main/java/nl/vyjy/controller/BordVakjeRepository.java
+++
@@ -1,9 +0,0 @@
-package nl.vyjy.controller;
-
-import org.springframework.data.repository.CrudRepository;
-
-import nl.vyjy.BordVakje;
-
-public interface BordVakjeRepository extends CrudRepository<BordVakje, Long> {
-
-}
